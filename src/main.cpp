c1d66f0c919b62a14419b02b64494cdef8ccb534
meindert <meindert.hoving@gmail.com>
2019-12-15 23:55:20 +0200

    Initital commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+int ledPin = 2;
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH);
+  delay(5000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+}
